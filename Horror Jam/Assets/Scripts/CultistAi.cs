a4860ee46899df004530b5f3da4259b038ca815f
CooperRIT <112188791+CooperRIT@users.noreply.github.com>
2024-06-07 13:03:56 -0500

    Cultist now patrols and spots, Lantern added to proper player model

--- Horror Jam/Assets/Scripts/CultistAi.cs
+++ Horror Jam/Assets/Scripts/CultistAi.cs
@@ -16,15 +16,36 @@ public class CultistAi : MonoBehaviour
     NavMeshAgent cultist;
 
     [Header("Patroling Variables")]
-    [SerializeField] Transform[] locationsToPatrol;
-    int currentPatrol;
+    [SerializeField] List<Vector3> locationsToPatrol = new List<Vector3>();
+    Transform transformList;
+    float minPatrolDistance = .5f;
+    bool reverse;
+    int currentPatrol = 0;
 
     [Header("SpottingVariables")]
-    float spottingTime;
+    [SerializeField] float timer;
+    float spottingTime = 3;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        cultist = GetComponentInParent<NavMeshAgent>();
+        transformList = transform.GetChild(0);
+
+        //Makes sure u have transform in the transform list
+        if(Application.isEditor && transformList.childCount == 0)
+        {
+            throw new System.Exception("you are a goober, populate the transform list");
+        }
+
+        locationsToPatrol.Add(transform.parent.position);
+        for (int i = 0; i < transformList.childCount; i++)
+        {
+            locationsToPatrol.Add(transformList.GetChild(i).position);
+        }
+
+        //When the cult member no longer needs their transform list, it destroys it
+        Destroy(transformList.gameObject);
+        currentState = CultistStates.Patroling;
     }
 
     // Update is called once per frame
@@ -45,12 +66,38 @@ public class CultistAi : MonoBehaviour
 
     void Patroling()
     {
-        cultist.SetDestination(locationsToPatrol[currentPatrol].position);
+        cultist.SetDestination(locationsToPatrol[currentPatrol]);
+
+        if (Vector3.Distance(locationsToPatrol[currentPatrol], cultist.transform.position) <= minPatrolDistance)
+        {
+            currentState = CultistStates.Spotting;
+        }
     }
 
     void Spotting()
     {
 
+        if (timer <= spottingTime)
+        {
+            timer += 1 * Time.deltaTime;
+            return;
+        }
+
+        //Switching back to patroling after finding nothing
+
+        if (currentPatrol == locationsToPatrol.Count - 1)
+        {
+            reverse = true;
+        }
+        else if(currentPatrol == 0)
+        {
+            reverse = false;
+        }
+
+        currentPatrol += reverse ? -1 : 1;
+
+        currentState = CultistStates.Patroling;
+        timer = 0f;
     }
 
     void Pursuing()
