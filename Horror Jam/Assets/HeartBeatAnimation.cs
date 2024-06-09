548b956d92ba8139886c610debeaf9815af20873
Cal2003 <C.Barnett2@uni.brighton.ac.uk>
2024-06-09 10:42:02 +0100

    Heart Beat
    Fine tuned the heart beat animation to be more realistic.

--- Horror Jam/Assets/HeartBeatAnimation.cs
+++ Horror Jam/Assets/HeartBeatAnimation.cs
@@ -2,11 +2,14 @@ using UnityEngine;
 
 public class HeartBeatAnimation : MonoBehaviour
 {
+
     private SkinnedMeshRenderer skinnedMeshRenderer;
 
     [SerializeField] private int blendShapeIndex = 0;
 
-    [SerializeField] private float beatSpeed = 1.0f;
+    [SerializeField] private float beatInterval = 1.0f;  
+    [SerializeField] private float beatDuration = 0.2f; 
+    [SerializeField] private float pauseDuration = 0.6f; 
     [SerializeField] private float maxBlendShapeValue = 100.0f;
 
     private float animationTime = 0.0f;
@@ -18,9 +21,33 @@ public class HeartBeatAnimation : MonoBehaviour
 
     void Update()
     {
-        animationTime += Time.deltaTime * beatSpeed;
-        float blendShapeValue = (Mathf.Sin(animationTime * Mathf.PI * 2) * 0.5f + 0.5f) * maxBlendShapeValue;
+        animationTime += Time.deltaTime;
+
+        float blendShapeValue = CalculateHeartbeat(animationTime);
 
         skinnedMeshRenderer.SetBlendShapeWeight(blendShapeIndex, blendShapeValue);
+
+        if (animationTime > beatInterval)
+        {
+            animationTime -= beatInterval;
+        }
+    }
+
+    float CalculateHeartbeat(float time)
+    {
+        float cyclePosition = time % beatInterval;
+
+        if (cyclePosition < beatDuration)
+        {
+            return Mathf.Sin(cyclePosition / beatDuration * Mathf.PI) * maxBlendShapeValue;
+        }
+        else if (cyclePosition < 2 * beatDuration)
+        {
+            return Mathf.Sin((cyclePosition - beatDuration) / beatDuration * Mathf.PI) * maxBlendShapeValue;
+        }
+        else
+        {
+            return 0.0f;
+        }
     }
 }
