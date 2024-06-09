cf66b7a0cebf0d2034f5b063c872e85544e69a36
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2024-06-09 18:19:15 -0400

    Fixed Player Interactor
    - Null check

--- Horror Jam/Assets/Scripts/Scriptable/UIEventChannel.cs
+++ Horror Jam/Assets/Scripts/Scriptable/UIEventChannel.cs
@@ -7,5 +7,5 @@ public class UIEventChannel : ScriptableObject
     public delegate void UpdatePrompt(string prompt);
     public event UpdatePrompt SetPrompt;
 
-    public void TriggerEvent(string prompt) => SetPrompt.Invoke(prompt);
+    public void TriggerEvent(string prompt) => SetPrompt?.Invoke(prompt);
 }
