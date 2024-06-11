fe609552c22741a8eaa5d7430a8878e2b862e57c
CooperRIT <112188791+CooperRIT@users.noreply.github.com>
2024-06-11 13:21:23 -0500

    Attempted to change a few things, but we are going with scortched earth with new solution

--- Horror Jam/Assets/Scripts/Scriptable/SoundEventChannel.cs
+++ Horror Jam/Assets/Scripts/Scriptable/SoundEventChannel.cs
@@ -6,12 +6,14 @@ using UnityEngine;
 public class SoundEventChannel : ScriptableObject
 {
     private float currentSoundLevel = 0;
+    float lastSoundLevel = 1;
     public float CurrentSoundLevel 
     {
         get {return Mathf.Clamp(currentSoundLevel, 0, 100); } 
-        set 
-        { 
+        set
+        {
             currentSoundLevel = value;
+            lastSoundLevel = value;
             currentSoundLevel = Mathf.Clamp(currentSoundLevel, 0, 100);
         }
     }
