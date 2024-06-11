883d5849516bf9a29b93601afcff955e5f8ee990
CooperRIT <112188791+CooperRIT@users.noreply.github.com>
2024-06-11 15:23:37 -0500

    Added animator

--- Horror Jam/Assets/Scripts/Valve.cs
+++ Horror Jam/Assets/Scripts/Valve.cs
@@ -6,6 +6,7 @@ public class Valve : MonoBehaviour, IInteract
 {
     [Header("Interact Settings")]
     [SerializeField] private string interactPrompt = "Hold [E] to close valve";
+    Animator animator;
     public string Prompt { get { return interactPrompt; } }
 
     private void Start()
