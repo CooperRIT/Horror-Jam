ad94b0172a7727c12621736485f82f862cde0436
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2024-06-14 23:42:20 -0400

    Completed Sound Meter
    - Lights change based on sound level (not representive of distance and will need tweaking)

--- Horror Jam/Assets/Scripts/SoundMeter.cs
+++ Horror Jam/Assets/Scripts/SoundMeter.cs
@@ -2,61 +2,91 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum LightStates
+{
+    Green,
+    Yellow,
+    Red,
+}
+
 public class SoundMeter : MonoBehaviour
 {
     [Header("Scriptable Object Reference")]
     [SerializeField] SoundEventChannel soundEventChannel;
 
 
+    LightStates state;
+
     [Header("Light Settings")]
     [SerializeField] List<Light> soundLights;
 
     [SerializeField] float blinkingInterval = 0.5f;
 
-    float blinkingTime;
+    int lightIndex;
 
     Light currentLight;
 
-    bool isSameLight = true;
+    bool isSameLight;
 
     float CurrentSoundLevel => soundEventChannel.CurrentSoundLevel;
 
     private void Start()
     {
-        currentLight = soundLights[0];
+        state = LightStates.Green;
     }
 
     private void Update()
     {
-        if (CurrentSoundLevel < 30f)
+        switch (state)
         {
-            currentLight = soundLights[0];
-            isSameLight = false;
-        }
-        else if (CurrentSoundLevel < 70)
-        {
-            currentLight = soundLights[1];
-            isSameLight = false;
-        }
-        else
-        {
-            currentLight = soundLights[2];
-            isSameLight = false;
+            case LightStates.Green:
+                if (CurrentSoundLevel >= 35f)
+                {
+                    state = LightStates.Yellow;
+                    isSameLight = false;
+                }
+                break;
+
+            case LightStates.Yellow:
+                if (CurrentSoundLevel >= 50f)
+                {
+                    state = LightStates.Red;
+                    isSameLight = false;
+                }   
+                else if (CurrentSoundLevel < 35f)
+                {
+                    state = LightStates.Green;
+                    isSameLight = false;
+                }
+                break;
+
+            case LightStates.Red:
+                if (CurrentSoundLevel < 50f)
+                {
+                    state = LightStates.Yellow;
+                    isSameLight = false;
+                }
+                break;
         }
 
         if (!isSameLight)
+        {
+            isSameLight = true;
             StartCoroutine(BlinkLight());
+        }
     }
 
     IEnumerator BlinkLight()
     {
-        isSameLight = true;
-        while (!isSameLight)
+        currentLight = soundLights[(int)state];
+
+        while (isSameLight)
         {
             currentLight.enabled = false;
             yield return new WaitForSeconds(blinkingInterval);
             currentLight.enabled = true;
             yield return new WaitForSeconds(blinkingInterval);
         }
+        currentLight.enabled = true;
     }
 }
